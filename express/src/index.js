e5bbe241c1ec6b6deb2b36d9d7397a912cfdab99
George Norfolk <g.norfolk@pageantmedia.com>
2023-05-14 21:08:39 +0100

    Moving index.js and removing /api and adding npm start for api.

--- express/src/index.js
+++ express/src/index.js
@@ -8,10 +8,6 @@ app.get("/url", (req, res, next) => {
     res.json(["Angela","Pam","Michael","Dwight","Kevin"]);
 });
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
-
 app.get("/test", (req, res, next) => {
     res.json("backend response")
 })
