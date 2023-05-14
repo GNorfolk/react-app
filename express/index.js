2f8072e1ec884aa7d146c1a7574ae7e11902f909
George Norfolk <g.norfolk@pageantmedia.com>
2023-05-14 18:30:49 +0100

    Adding express node server backend and moving to distinct folders.

---
+++ express/index.js
@@ -0,0 +1,17 @@
+let express = require("express");
+
+let app = express();app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
+
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.get("/test", (req, res, next) => {
+    res.json("backend response")
+})
