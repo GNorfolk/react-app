112eafae11da8730e1b1d7961c96805f68a7826b
George Norfolk <g.norfolk@pageantmedia.com>
2023-06-16 01:54:02 +0100

    Adding handling for image optimisation as per doc in readme.

--- next/tf/cloudfront.tf
+++ next/tf/cloudfront.tf
@@ -11,6 +11,42 @@ resource "aws_cloudfront_distribution" "this" {
   enabled = true
   price_class = "PriceClass_100"
 
+  origin_group {
+    origin_id = "s3image"
+    failover_criteria {
+      status_codes = [403]
+    }
+    member {
+      origin_id = "s3transformed"
+    }
+    member {
+      origin_id = "imagelambda"
+    }
+  }
+
+  origin {
+    domain_name = aws_s3_bucket.transformed.bucket_regional_domain_name
+    origin_id   = "s3transformed"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
+    }
+  }
+
+  origin {
+    domain_name = element(split("/", aws_lambda_function_url.image.function_url), length(split("/", aws_lambda_function_url.image.function_url)) - 2)
+    origin_id   = "imagelambda"
+    custom_header {
+      name = "x-origin-secret-header" 
+      value = "test"
+    }
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+  }
+
   origin {
     domain_name              = aws_s3_bucket.this.bucket_domain_name
     origin_id                = aws_cloudfront_origin_access_identity.this.id
@@ -29,16 +65,37 @@ resource "aws_cloudfront_distribution" "this" {
     default_ttl = 3600
     max_ttl = 31536000
     forwarded_values {
-      query_string = true
-      headers = ["Origin"]
+      query_string = false
       cookies {
-        forward = "all"
+        forward = "none"
       }
     }
     lambda_function_association {
       event_type = "origin-request"
       lambda_arn = aws_lambda_function.this.qualified_arn
     }
+  }
+
+  ordered_cache_behavior {
+    path_pattern = "_next/image"
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "s3image"
+    min_ttl = 0
+    default_ttl = 86400
+    max_ttl = 31536000
+    compress = true
+    viewer_protocol_policy = "redirect-to-https"
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
+    # lambda_function_association {
+    #   event_type = "viewer-request"
+    #   lambda_arn = "${aws_cloudfront_function.this.arn}:1"
+    # }
   }
 
   ordered_cache_behavior {
@@ -50,42 +107,6 @@ resource "aws_cloudfront_distribution" "this" {
     default_ttl = 86400
     max_ttl = 86400
     compress = true
-    viewer_protocol_policy = "https-only"
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  ordered_cache_behavior {
-    path_pattern = "static/*"
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
-    target_origin_id = aws_cloudfront_origin_access_identity.this.id
-    min_ttl = 86400
-    default_ttl = 86400
-    max_ttl = 86400
-    compress = true
-    viewer_protocol_policy = "https-only"
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  ordered_cache_behavior {
-    path_pattern = "tests/*"
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
-    target_origin_id = aws_cloudfront_origin_access_identity.this.id
-    min_ttl = 0
-    default_ttl = 0
-    max_ttl = 0
-    compress = false
     viewer_protocol_policy = "https-only"
     forwarded_values {
       query_string = false
---
+++ next/tf/image.tf
@@ -0,0 +1,105 @@
+resource "aws_lambda_function" "image" {
+  function_name = "react-app-image-function"
+  filename = "index.zip"
+  role = aws_iam_role.image.arn
+  handler = "index.handler"
+  runtime = "nodejs16.x"
+  timeout = 60
+  environment {
+    variables = {
+      originalImageBucketName = aws_s3_bucket.original.id
+      transformedImageBucketName = aws_s3_bucket.transformed.id
+      transformedImageCacheTTL = "max-age=31622400"
+      secretKey = "test"
+      logTiming = "false"
+    }
+  }
+}
+
+resource "aws_lambda_function_url" "image" {
+  function_name = aws_lambda_function.image.function_name
+  authorization_type = "NONE"
+}
+
+data "archive_file" "image" {
+  type = "zip"
+  source_file = "index.js"
+  output_path = "index.zip"
+}
+
+resource "aws_s3_bucket" "original" {
+  bucket = "klofron-nextjs-image-original"
+}
+
+resource "aws_s3_bucket_public_access_block" "original" {
+  bucket = aws_s3_bucket.original.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "transformed" {
+  bucket = "klofron-nextjs-image-transformed"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "transformed" {
+  bucket = aws_s3_bucket.transformed.id
+  rule {
+    id = "rule-1"
+    status = "Enabled"
+    expiration {
+      days = 90
+    }
+  }
+}
+
+data "aws_iam_policy_document" "image" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.original.arn}/*"
+    ]
+  }
+  statement {
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.transformed.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_role" "image" {
+  name = "react-app-image-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          "Service": [
+            "lambda.amazonaws.com",
+            "edgelambda.amazonaws.com"
+          ]
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "image" {
+  role = aws_iam_role.image.name
+  policy = data.aws_iam_policy_document.image.json
+}
+
+resource "aws_cloudfront_function" "this" {
+  name = "url-rewrite"
+  runtime = "cloudfront-js-1.0"
+  publish = true
+  code = file("${path.module}/url-rewrite.js")
+}
