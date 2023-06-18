fd432a77babcc1ed2c6ac7de0707c3d44c55dbd6
George Norfolk <g.norfolk@pageantmedia.com>
2023-06-18 19:02:12 +0100

    Setting up scheduler for schedule lambda.

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -50,3 +50,22 @@ resource "aws_iam_role_policy" "this" {
     ]
   })
 }
+
+resource "aws_cloudwatch_event_rule" "this" {
+  name = "schedule-rds-rule"
+  schedule_expression = "cron(0 1 ? * * *)"
+}
+
+resource "aws_cloudwatch_event_target" "this" {
+  rule = aws_cloudwatch_event_rule.this.name
+  target_id = "schedule-rds-target"
+  arn = aws_lambda_function.this.arn
+}
+
+resource "aws_lambda_permission" "this" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.this.arn
+}
